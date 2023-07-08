b9c23856729c9de784768b2867a3fa3a4196e210
Barry Fitzachary <Barryfitzachary@gmail.com>
2023-07-08 23:44:59 +0000

    fixed incorrect data in js file to have js file data

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,27 +1,98 @@
-CSSConditionRule.log
+// Initialize variables
+let score = 0;
+let tries = 5;
 
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Rock, Paper, Scissors, Lizard, Spock Game</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-</head>
-<body>
-  <h1>Rock, Paper, Scissors, Lizard, Spock</h1>
-  <div id="game">
-    <button class="choice" id="rock"><i class="fas fa-hand-rock"></i> Rock</button>
-    <button class="choice" id="paper"><i class="fas fa-hand-paper"></i> Paper</button>
-    <button class="choice" id="scissors"><i class="fas fa-hand-scissors"></i> Scissors</button>
-    <button class="choice" id="lizard"><i class="fas fa-hand-lizard"></i> Lizard</button>
-    <button class="choice" id="spock"><i class="fas fa-hand-spock"></i> Spock</button>
+// Get DOM elements
+const choices = document.querySelectorAll('.choice');
+const message = document.getElementById('message');
+const scoreDisplay = document.getElementById('score');
+const triesDisplay = document.getElementById('tries');
 
-  </div>
-  <div id="result">
-    <h2 id="message">Make your move!</h2>
-    <p id="score">Score: 0</p>
-    <p id="tries">Tries left: 5</p>
-  </div>
-  <script src="assets/JS/script.js"></script>
-</body>
-</html>
+// Add event listeners to choices
+choices.forEach(choice => {
+  choice.addEventListener('click', playGame);
+});
+
+// Game logic 
+function playGame(event) {
+  if (tries === 0) {
+    message.textContent = 'Game over! You ran out of tries.';
+    return;
+  }
+
+  const userChoice = event.target.id;
+  const computerChoice = generateComputerChoice();
+
+  const result = checkResult(userChoice, computerChoice);
+  updateUI(userChoice, computerChoice, result);
+
+  tries--;
+  triesDisplay.textContent = `Tries left: ${tries}`;
+
+  if (tries === 0) {
+    endGame();
+  }
+}
+
+// Generate computer's choice
+function generateComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+// Check the result
+function checkResult(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "It's a tie, come on, you can do this!!";
+  }
+
+  switch (userChoice) {
+    case 'rock':
+      return computerChoice === 'scissors' || computerChoice === 'lizard'
+        ? 'You win!'
+        : 'You lose!';
+    case 'paper':
+      return computerChoice === 'rock' || computerChoice === 'spock'
+        ? 'You win!'
+        : 'You lose!';
+    case 'scissors':
+      return computerChoice === 'paper' || computerChoice === 'lizard'
+        ? 'You win!'
+        : 'You lose!';
+    case 'lizard':
+      return computerChoice === 'paper' || computerChoice === 'spock'
+        ? 'You win!'
+        : 'You lose!';
+    case 'spock':
+      return computerChoice === 'rock' || computerChoice === 'scissors'
+        ? 'You win!'
+        : 'You lose!';
+    default:
+      return 'Invalid choice.';
+  }
+}
+
+// Initialize variables
+let userScore = 0;
+let computerScore = 0;
+
+// Update the UI
+function updateUI(userChoice, computerChoice, result) {
+  message.textContent = `You chose ${userChoice}. Computer chose ${computerChoice}. ${result}`;
+  if (result === 'You win!') {
+    userScore++;
+  } else if (result === 'You lose!') {
+    computerScore++;
+  }
+  scoreDisplay.textContent = `Score: You ${userScore} - Computer ${computerScore}`;
+}
+
+
+
+// End the game
+function endGame() {
+  choices.forEach(choice => {
+    choice.disabled = true;
+  });
+}
